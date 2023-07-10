f36a30066139c0cae76c51526569b75130b4a32c
François Garillot <4142+huitseeker@users.noreply.github.com>
2023-07-10 15:23:02 -0400

    Witness generation (ICFP benchmarks, on dev) (#478)
    * Use neptune witness-generation.
    
    * Add itcalc example.
    
    * fix: adapt end2end benchmark & associated files
    
    * Refactor version handling and vergen version
    
    - Overhauled the method of obtaining commit date and short sha across files by replacing module functions with environment variables.
    - Upgraded the `vergen` dependency from version `0.1` to `8` in `Cargo.toml` and imported limited features.
    
    * fix: restore test_prove_fibonacci
    
    * test: refactor mismatch test utility
    
    * chore:cleanup
    
    ---------
    
    Co-authored-by: porcuquine <porcuquine@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,10 @@ use anyhow::Result;
 
 fn main() -> Result<()> {
     pretty_env_logger::init();
+    println!(
+        "commit: {} {}",
+        env!("VERGEN_GIT_COMMIT_DATE"),
+        env!("VERGEN_GIT_SHA")
+    );
     cli::parse_and_run()
 }
